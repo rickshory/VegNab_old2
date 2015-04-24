12b22823437405281948f59f80fc4fd465477cbb
Rick Shory <rickshory@gmail.com>
2015-04-24 09:26:19 -0700

    move TrackerName enum from Activity to Application

--- app/src/main/java/com/vegnab/vegnab/MainVNActivity.java
+++ app/src/main/java/com/vegnab/vegnab/MainVNActivity.java
@@ -58,21 +58,6 @@ public class MainVNActivity extends ActionBarActivity
 	
 	ViewPager viewPager = null;
 
-    /**
-     * Enum used to identify the tracker that needs to be used for tracking.
-     *
-     * A single tracker is usually enough for most purposes. In case you do need multiple trackers,
-     * storing them all in Application object helps ensure that they are created only once per
-     * application instance.
-     */
-    public enum TrackerName {
-        APP_TRACKER, // Tracker used only in this app.
-        GLOBAL_TRACKER, // Tracker used by all the apps from RickShoryDotCom. eg: roll-up tracking.
-        ECOMMERCE_TRACKER, // Tracker used by all ecommerce transactions from RickShoryDotCom.
-    }
-
-    HashMap<TrackerName, Tracker> mTrackers = new HashMap<TrackerName, Tracker>();
-
     @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
--- app/src/main/java/com/vegnab/vegnab/VNApplication.java
+++ app/src/main/java/com/vegnab/vegnab/VNApplication.java
@@ -1,7 +1,23 @@
 package com.vegnab.vegnab;
 
 import android.app.Application;
+import com.google.android.gms.analytics.Tracker;
+import java.util.HashMap;
 
 public class VNApplication extends Application {
+    /**
+     * Enum used to identify the tracker that needs to be used for tracking.
+     *
+     * A single tracker is usually enough for most purposes. In case you do need multiple trackers,
+     * storing them all in Application object helps ensure that they are created only once per
+     * application instance.
+     */
+    public enum TrackerName {
+        APP_TRACKER, // Tracker used only in this app.
+        GLOBAL_TRACKER, // Tracker used by all the apps from RickShoryDotCom. eg: roll-up tracking.
+        ECOMMERCE_TRACKER, // Tracker used by all ecommerce transactions from RickShoryDotCom.
+    }
+
+    HashMap<TrackerName, Tracker> mTrackers = new HashMap<TrackerName, Tracker>();
 
 }
