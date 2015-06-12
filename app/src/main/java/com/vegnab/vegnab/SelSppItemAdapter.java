11629dde0e2ff5ebfd3a7759e97ab7b88ac02edf
Rick Shory <rickshory@gmail.com>
2015-06-12 10:38:48 -0700

    background colors in spp matcher; not sure this is the way to go

--- app/src/main/java/com/vegnab/vegnab/SelSppItemAdapter.java
+++ app/src/main/java/com/vegnab/vegnab/SelSppItemAdapter.java
@@ -27,8 +27,36 @@ public class SelSppItemAdapter extends ResourceCursorAdapter {
 //		}
         TextView sppText = (TextView) v.findViewById(R.id.spp_descr_text);
         sppText.setText(c.getString(c.getColumnIndexOrThrow("MatchTxt")));
-        if (c.getInt(c.getColumnIndexOrThrow("IsPlaceholder")) == 1) {
-            v.setBackgroundColor(ctx.getResources().getColor(R.color.vn_color));
+        int lstN = c.getInt(c.getColumnIndexOrThrow("SubListOrder"));
+        int isPh = c.getInt(c.getColumnIndexOrThrow("IsPlaceholder"));
+        if (isPh == 1) { // a placeholder, assign a color for easier spotting
+            if (lstN == 1) {
+                v.setBackgroundColor(ctx.getResources().getColor(R.color.match_placeholder_code));
+            } else {
+                v.setBackgroundColor(ctx.getResources().getColor(R.color.match_placeholder_text));
+            }
+        } else { // not a placeholder, assign distinctive colors
+            switch (lstN) {
+                case 1: // a previously found code
+                    v.setBackgroundColor(ctx.getResources().getColor(R.color.match_previously_found_species_code));
+                    break;
+                case 2: // a previously found code, best match
+                    v.setBackgroundColor(ctx.getResources().getColor(R.color.match_previously_found_species_text));
+                    break;
+                case 3: // not previously found but a local species, match by code
+                    v.setBackgroundColor(ctx.getResources().getColor(R.color.match_local_species_code));
+                    break;
+                case 4: // not previously found but a local species, match by text
+                    v.setBackgroundColor(ctx.getResources().getColor(R.color.match_local_species_text));
+                    break;
+                case 5: // not a local species, match by code, low priority
+                    v.setBackgroundColor(ctx.getResources().getColor(R.color.match_nonlocal_species_code));
+                    break;
+                case 6: // not a local species, match by text, lowest priority
+                    v.setBackgroundColor(ctx.getResources().getColor(R.color.match_nonlocal_species_text));
+                    break;
+
+            }
         }
     }
 }
