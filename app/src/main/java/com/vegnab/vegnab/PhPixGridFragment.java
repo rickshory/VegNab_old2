b100b911e5084b7d00996a408da93fdf6e217efd
Rick Shory <rickshory@gmail.com>
2015-06-24 10:22:48 -0700

    work on Placeholder pix; PhPixGridFragment still incomplete

---
+++ app/src/main/java/com/vegnab/vegnab/PhPixGridFragment.java
@@ -0,0 +1,62 @@
+package com.vegnab.vegnab;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+public class PhPixGridFragment extends Fragment {
+
+    final static String ARG_PLACEHOLDER_ID = "phId";
+    private GridView mPhPixGridView;
+    private PhPixGridAdapter mPhPixGridAdapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        // if the activity was re-created (e.g. from a screen rotate)
+        // restore the previous parameters remembered by onSaveInstanceState()
+        // This is mostly needed in fixed-pane layouts
+/*		if (savedInstanceState != null) {
+            // restore parameters
+            }
+        } else {
+            // default parameters
+        } */
+        // inflate the layout for this fragment
+
+        View rootView = inflater.inflate(R.layout.fragment_ph_pix_grid, container, false);
+        mPhPixGridView = (GridView) rootView.findViewById(R.id.phPixGridView);
+        //mPhPixGridAdapter = new PhPixGridAdapter(this, R.layout.grid_item_layout, getData());
+        mPhPixGridAdapter = new PhPixGridAdapter(getActivity(), R.layout.grid_ph_pix, null, 0);
+        mPhPixGridView.setAdapter(mPhPixGridAdapter);
+
+        return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // during startup, check if arguments are passed to the fragment
+        // this is where to do this because the layout has been applied
+        // to the fragment
+        Bundle args = getArguments();
+        if (args != null) {
+//            start loader to populate grid
+//            mTxtNote.setText(args.getString(ARG_NOTE_ID));
+        }
+    }
+}
+
+/* adapt following to work with cursor rather than array
+    private ArrayList<ImageItem> getData() {
+        final ArrayList<ImageItem> imageItems = new ArrayList<>();
+        TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
+        for (int i = 0; i < imgs.length(); i++) {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i, -1));
+            imageItems.add(new ImageItem(bitmap, "Image#" + i));
+        }
+        return imageItems;
+*/
