c8b86253dcbe5627982ffffe4e2afdf438c20349
Rick Shory <rickshory@gmail.com>
2015-06-27 08:19:56 -0700

    in PhPixGridFragment, sketched in Loader methods

--- app/src/main/java/com/vegnab/vegnab/PhPixGridFragment.java
+++ app/src/main/java/com/vegnab/vegnab/PhPixGridFragment.java
@@ -1,15 +1,28 @@
 package com.vegnab.vegnab;
 
+import android.content.ContentUris;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
-public class PhPixGridFragment extends Fragment {
+import com.vegnab.vegnab.contentprovider.ContentProvider_VegNab;
+import com.vegnab.vegnab.database.VNContract;
 
+public class PhPixGridFragment extends Fragment
+        implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final String LOG_TAG = PhPixGridFragment.class.getSimpleName();
     final static String ARG_PLACEHOLDER_ID = "phId";
+    long mPlaceholderId;
     private GridView mPhPixGridView;
     private PhPixGridAdapter mPhPixGridAdapter;
 
@@ -21,7 +34,7 @@ public class PhPixGridFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+                             Bundle savedInstanceState) {
         // if the activity was re-created (e.g. from a screen rotate)
         // restore the previous parameters remembered by onSaveInstanceState()
         // This is mostly needed in fixed-pane layouts
@@ -54,15 +67,71 @@ public class PhPixGridFragment extends Fragment {
 //            mTxtNote.setText(args.getString(ARG_NOTE_ID));
         }
     }
-}
 
-/* adapt following to work with cursor rather than array
-    private ArrayList<ImageItem> getData() {
-        final ArrayList<ImageItem> imageItems = new ArrayList<>();
-        TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
-        for (int i = 0; i < imgs.length(); i++) {
-            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i, -1));
-            imageItems.add(new ImageItem(bitmap, "Image#" + i));
+
+    /* adapt following to work with cursor rather than array
+        private ArrayList<ImageItem> getData() {
+            final ArrayList<ImageItem> imageItems = new ArrayList<>();
+            TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
+            for (int i = 0; i < imgs.length(); i++) {
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i, -1));
+                imageItems.add(new ImageItem(bitmap, "Image#" + i));
+            }
+            return imageItems;
+    */
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // This is called when a new Loader needs to be created.
+        // switch out based on id
+        CursorLoader cl = null;
+        Uri baseUri;
+        String select = null; // default for all-columns, unless re-assigned or overridden by raw SQL
+        switch (id) {
+            case VNContract.Loaders.PLACEHOLDER_TO_EDIT:
+                Uri onePlaceholderUri = ContentUris.withAppendedId(
+                        Uri.withAppendedPath(
+                                ContentProvider_VegNab.CONTENT_URI, "placeholders"), mPlaceholderId);
+                cl = new CursorLoader(getActivity(), onePlaceholderUri,
+                        null, select, null, null);
+                break;
         }
-        return imageItems;
-*/
+        return cl;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor c) {
+        // there will be various loaders, switch them out here
+//        mRowCt = c.getCount();
+        switch (loader.getId()) {
+
+            case VNContract.Loaders.PLACEHOLDER_TO_EDIT:
+                Log.d(LOG_TAG, "onLoadFinished, PLACEHOLDER_TO_EDIT, records: " + c.getCount());
+                if (c.moveToFirst()) {
+    //				mPlaceholderId = c.getLong(c.getColumnIndexOrThrow("_id"));
+//                    mViewPlaceholderCode.setText(c.getString(c.getColumnIndexOrThrow("PlaceHolderCode")));
+//                    mViewPlaceholderDescription.setText(c.getString(c.getColumnIndexOrThrow("Description")));
+//                    mViewPlaceholderHabitat.setText(c.getString(c.getColumnIndexOrThrow("Habitat")));
+//                    mViewPlaceholderIdentifier.setText(c.getString(c.getColumnIndexOrThrow("LabelNum")));
+    //				mPhVisitId = c.getLong(c.getColumnIndexOrThrow("VisitIdWhereFirstFound"));
+    //				mPhProjId = c.getLong(c.getColumnIndexOrThrow("ProjID"));
+    //				mPhNamerId = c.getLong(c.getColumnIndexOrThrow("NamerID"));
+                } else { // no record to edit yet, set up new record
+//                    mViewPlaceholderCode.setText(mPlaceholderCode);
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // This is called when the last Cursor provided to onLoadFinished()
+        // is about to be closed. Need to make sure it is no longer is use.
+        switch (loader.getId()) {
+
+        case VNContract.Loaders.PLACEHOLDER_TO_EDIT:
+            Log.d(LOG_TAG, "onLoaderReset, PLACEHOLDER_TO_EDIT.");
+//			don't need to do anything here, no cursor adapter
+            break;
+        }
+    }
+}
