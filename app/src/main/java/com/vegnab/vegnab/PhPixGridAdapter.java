66ad6f0fe2686d6b21417ed18a9fcecc015bb2ef
Rick Shory <rickshory@gmail.com>
2015-07-04 06:49:32 -0700

    pix grid adapter, compiles but still crashes

--- app/src/main/java/com/vegnab/vegnab/PhPixGridAdapter.java
+++ app/src/main/java/com/vegnab/vegnab/PhPixGridAdapter.java
@@ -46,52 +46,56 @@ public class PhPixGridAdapter extends ResourceCursorAdapter {
     }
 }*/
 
-	private Context context;
-	private int layoutResourceId;
-	private ArrayList data = new ArrayList();
+//	private Context context;
+//	private int layoutResourceId;
+//	private ArrayList data = new ArrayList();
+//
+//	public GridViewAdapter(Context context, int layoutResourceId, ArrayList data) {
+//		super(context, layoutResourceId, data);
+//		this.layoutResourceId = layoutResourceId;
+//		this.context = context;
+//		this.data = data;
+//	}
+//
+    // break the following apart to use with cursor
+//	@Override
+//	public View getView(int position, View convertView, ViewGroup parent) {
+//		View row = convertView;
+//		ViewHolder holder = null;
+//
+    // adapt the following for newView
+//		if (row == null) {
+//			LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+//			row = inflater.inflate(layoutResourceId, parent, false);
+//			holder = new ViewHolder();
+//			holder.imageTitle = (TextView) row.findViewById(R.id.phGridItemText);
+//			holder.image = (ImageView) row.findViewById(R.id.phGridItemImage);
+//			row.setTag(holder);
+//		} else {
+    // adapt the following for bindView
+//			holder = (ViewHolder) row.getTag();
+//		}
+//
+//		VNContract.VNGridImageItem item = data.get(position);
+//		holder.imageTitle.setText(item.getTitle());
+//		holder.image.setImageBitmap(item.getImage());
+//		return row;
+//	}
+//
+//	static class ViewHolder {
+//		TextView imageTitle;
+//		ImageView image;
+//	}
 
-	public GridViewAdapter(Context context, int layoutResourceId, ArrayList data) {
-		super(context, layoutResourceId, data);
-		this.layoutResourceId = layoutResourceId;
-		this.context = context;
-		this.data = data;
-	}
-
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		View row = convertView;
-		ViewHolder holder = null;
-
-		if (row == null) {
-			LayoutInflater inflater = ((Activity) context).getLayoutInflater();
-			row = inflater.inflate(layoutResourceId, parent, false);
-			holder = new ViewHolder();
-			holder.imageTitle = (TextView) row.findViewById(R.id.phGridItemText);
-			holder.image = (ImageView) row.findViewById(R.id.phGridItemImage);
-			row.setTag(holder);
-		} else {
-			holder = (ViewHolder) row.getTag();
-		}
-
-		VNContract.VNGridImageItem item = data.get(position);
-		holder.imageTitle.setText(item.getTitle());
-		holder.image.setImageBitmap(item.getImage());
-		return row;
-	}
-
-	static class ViewHolder {
-		TextView imageTitle;
-		ImageView image;
-	}
-}
     @Override
     public void bindView(View v, Context ctx, Cursor c) {
-// example of formatting by position
-//		if(c.getPosition()%2==1) {
-//			view.setBackgroundColor(ctx.getResources().getColor(R.color.background_odd));
-//		} else {
-//			view.setBackgroundColor(ctx.getResources().getColor(R.color.background_even));
-//		}
+        TextView phGridCellText = (TextView) v.findViewById(R.id.phGridItemText);
+        String note = c.getString(c.getColumnIndexOrThrow("PhotoNotes"));
+        if (note == null) {
+            note = "(no note)";
+        }
+        phGridCellText.setText(note);
+        ImageView phGridCellImage = (ImageView) v.findViewById(R.id.phGridItemImage);
 
 /*CREATE TABLE IF NOT EXISTS "PlaceHolderPix" (
 "_id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
@@ -100,16 +104,12 @@ public class PhPixGridAdapter extends ResourceCursorAdapter {
 "PhotoTimeStamp" TIMESTAMP NOT NULL DEFAULT (DATETIME('now')),
 "PhotoNotes" VARCHAR(255),
 "PhotoURL" VARCHAR(255),*/
-        String note = c.getString(c.getColumnIndexOrThrow("PhotoNotes"));
-        if (note == null) {
-            note = "(no note)";
-        }
         String path = c.getString(c.getColumnIndexOrThrow("PhotoPath"));
         File imgFile = new  File(path);
         if (imgFile.exists()) {
             Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-            mImgOriginal.setImageBitmap(myBitmap);
-            mImgOriginal.setAdjustViewBounds(true);
+            phGridCellImage.setImageBitmap(myBitmap);
+            phGridCellImage.setAdjustViewBounds(true);
         }
 
 
