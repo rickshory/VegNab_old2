8a340980da53218acaf912b81a29c993ab8b0d66
Rick Shory <rickshory@gmail.com>
2015-08-23 12:23:43 -0700

    Drive API create-file sample code pasted in, still commented out

--- app/src/main/java/com/vegnab/vegnab/MainVNActivity.java
+++ app/src/main/java/com/vegnab/vegnab/MainVNActivity.java
@@ -1026,6 +1026,68 @@ public class MainVNActivity extends ActionBarActivity
 
     // end of Google Drive API boilerplate
 
+    /*
+
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        super.onConnected(connectionHint);
+        // create new contents resource
+        Drive.DriveApi.newDriveContents(getGoogleApiClient())
+                .setResultCallback(driveContentsCallback);
+    }
+
+    final private ResultCallback<DriveContentsResult> driveContentsCallback = new
+            ResultCallback<DriveContentsResult>() {
+        @Override
+        public void onResult(DriveContentsResult result) {
+            if (!result.getStatus().isSuccess()) {
+                showMessage("Error while trying to create new file contents");
+                return;
+            }
+            final DriveContents driveContents = result.getDriveContents();
+
+            // Perform I/O off the UI thread.
+            new Thread() {
+                @Override
+                public void run() {
+                    // write content to DriveContents
+                    OutputStream outputStream = driveContents.getOutputStream();
+                    Writer writer = new OutputStreamWriter(outputStream);
+                    try {
+                        writer.write("Hello World!");
+                        writer.close();
+                    } catch (IOException e) {
+                        Log.e(TAG, e.getMessage());
+                    }
+
+                    MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
+                            .setTitle("New file")
+                            .setMimeType("text/plain")
+                            .setStarred(true).build();
+
+                    // create a file on root folder
+                    Drive.DriveApi.getRootFolder(getGoogleApiClient())
+                            .createFile(getGoogleApiClient(), changeSet, driveContents)
+                            .setResultCallback(fileCallback);
+                }
+            }.start();
+        }
+    };
+
+    final private ResultCallback<DriveFileResult> fileCallback = new
+            ResultCallback<DriveFileResult>() {
+        @Override
+        public void onResult(DriveFileResult result) {
+            if (!result.getStatus().isSuccess()) {
+                showMessage("Error while trying to create the file");
+                return;
+            }
+            showMessage("Created a file with content: " + result.getDriveFile().getDriveId());
+        }
+    };
+*/
+
 
 /*
     @Override
