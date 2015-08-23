7bd26ba6207aace25bf96bd32a8acfadfd862688
Rick Shory <rickshory@gmail.com>
2015-08-23 12:22:35 -0700

    Google Drive API boilerplate

--- app/src/main/java/com/vegnab/vegnab/MainVNActivity.java
+++ app/src/main/java/com/vegnab/vegnab/MainVNActivity.java
@@ -13,7 +13,10 @@ import java.util.UUID;
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
 import com.vegnab.vegnab.BuildConfig;
 import com.vegnab.vegnab.contentprovider.ContentProvider_VegNab;
 import com.vegnab.vegnab.database.VNContract;
@@ -22,6 +25,8 @@ import com.vegnab.vegnab.database.VNContract.Tags;
 
 import android.app.AlertDialog;
 import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -64,7 +69,9 @@ public class MainVNActivity extends ActionBarActivity
         EditSppItemDialog.EditSppItemDialogListener,
         EditPlaceholderFragment.OnButtonListener,
         ConfirmDelVegItemDialog.ConfirmDeleteVegItemDialogListener,
-        UnHideVisitDialog.ConfirmUnHideVisitDialogListener {
+        UnHideVisitDialog.ConfirmUnHideVisitDialogListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
         // LoaderManager.LoaderCallbacks<Cursor>
 
     private static final String LOG_TAG = MainVNActivity.class.getSimpleName();
@@ -925,6 +932,101 @@ public class MainVNActivity extends ActionBarActivity
         }
     }
     */
+
+    // Google Drive API boilerplate, could be in separate class
+
+    // Request code for auto Google Play Services error resolution.
+    protected static final int REQUEST_CODE_RESOLUTION = 1;
+    // Next available request code.
+    protected static final int NEXT_AVAILABLE_REQUEST_CODE = 2;
+    // Google API client.
+    private GoogleApiClient mGoogleApiClient;
+
+
+// Called when activity gets visible. A connection to Drive services need to
+// be initiated as soon as the activity is visible. Registers
+// {@code ConnectionCallbacks} and {@code OnConnectionFailedListener} on the
+// activities itself.
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(Drive.API)
+                    .addScope(Drive.SCOPE_FILE)
+                    .addScope(Drive.SCOPE_APPFOLDER) // required for App Folder sample
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+        }
+        mGoogleApiClient.connect();
+    }
+
+    // Handles resolution callbacks.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_RESOLUTION && resultCode == RESULT_OK) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    // Called when activity gets invisible. Connection to Drive service needs to
+    // be disconnected as soon as an activity is invisible.
+    @Override
+    protected void onPause() {
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.disconnect();
+        }
+        super.onPause();
+    }
+
+    /**
+     * Called when {@code mGoogleApiClient} is connected.
+     */
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.i(LOG_TAG, "GoogleApiClient connected");
+    }
+
+    // Called when {@code mGoogleApiClient} is disconnected
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.i(LOG_TAG, "GoogleApiClient connection suspended");
+    }
+
+    // Called when {@code mGoogleApiClient} is trying to connect but failed.
+    // Handle {@code result.getResolution()} if there is a resolution is available.
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i(LOG_TAG, "GoogleApiClient connection failed: " + result.toString());
+        if (!result.hasResolution()) {
+            // show the localized error dialog.
+            GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), this, 0).show();
+            return;
+        }
+        try {
+            result.startResolutionForResult(this, REQUEST_CODE_RESOLUTION);
+        } catch (IntentSender.SendIntentException e) {
+            Log.e(LOG_TAG, "Exception while starting resolution activity", e);
+        }
+    }
+
+    // Shows a toast message.
+    public void showMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    // Getter for the {@code GoogleApiClient}.
+    public GoogleApiClient getGoogleApiClient() {
+        return mGoogleApiClient;
+    }
+
+    // end of Google Drive API boilerplate
+
+
 /*
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
