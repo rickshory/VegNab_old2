fa16e367104bc22b3603cc8390bc91f307832a28
Rick Shory <rickshory@gmail.com>
2015-12-02 09:16:01 -0800

    more diagnostics on Drive completion

--- app/src/main/java/com/vegnab/vegnab/VNDriveEventService.java
+++ app/src/main/java/com/vegnab/vegnab/VNDriveEventService.java
@@ -14,18 +14,28 @@ public class VNDriveEventService extends DriveEventService {
     private static final String LOG_TAG = VNDriveEventService.class.getSimpleName();
 
       @Override
-  public void onCompletion(CompletionEvent event) {
-          if (LDebug.ON) Log.d(LOG_TAG, "XXX in onCompletion before super");
-          if (LDebug.ON) Log.d(LOG_TAG, "XXX Drive Action completed with status: " + event.getStatus());
-    super.onCompletion(event);
-          if (LDebug.ON) Log.d(LOG_TAG, "XXX in onCompletion after super");
-          if (LDebug.ON) Log.d(LOG_TAG, "XXX Drive Action completed with status: " + event.getStatus());
-    DriveId driveId = event.getDriveId();
-    if (LDebug.ON) Log.d(LOG_TAG, "XXX onComplete: " + driveId.getResourceId());
-    switch (event.getStatus()) {
-      case CompletionEvent.STATUS_CONFLICT:  if (LDebug.ON) Log.d(LOG_TAG, "XXX STATUS_CONFLICT"); event.dismiss(); break;
-      case CompletionEvent.STATUS_FAILURE:   if (LDebug.ON) Log.d(LOG_TAG, "XXX STATUS_FAILURE");  event.dismiss(); break;
-      case CompletionEvent.STATUS_SUCCESS:   if (LDebug.ON) Log.d(LOG_TAG, "XXX STATUS_SUCCESS "); event.dismiss(); break;
+    public void onCompletion(CompletionEvent event) {
+        super.onCompletion(event);
+        DriveId driveId = event.getDriveId();
+        if (LDebug.ON) Log.d(LOG_TAG, "XXX Resource ID: " + driveId.getResourceId()); // returns the final part of the URL
+        if (LDebug.ON) Log.d(LOG_TAG, "XXX Tracking Tags: " + event.getTrackingTags()); // first one is tracking tag sent
+        if (LDebug.ON) Log.d(LOG_TAG, "XXX Account Name: " + event.getAccountName()); // returns null
+        if (LDebug.ON) Log.d(LOG_TAG, "XXX DriveID: " + event.getDriveId()); // returns cryptic string something like expanded form of Drive ID sent
+        if (LDebug.ON) Log.d(LOG_TAG, "XXX toString: " + event.toString()); // returns DriveID, status, and trackingTag
+        if (LDebug.ON) Log.d(LOG_TAG, "XXX describe: " + event.describeContents()); // returns zero
+        switch (event.getStatus()) {
+            case CompletionEvent.STATUS_CONFLICT:
+                if (LDebug.ON) Log.d(LOG_TAG, "XXX STATUS_CONFLICT");
+                event.dismiss();
+                break;
+            case CompletionEvent.STATUS_FAILURE:
+                if (LDebug.ON) Log.d(LOG_TAG, "XXX STATUS_FAILURE");
+                event.dismiss();
+                break;
+            case CompletionEvent.STATUS_SUCCESS:
+                if (LDebug.ON) Log.d(LOG_TAG, "XXX STATUS_SUCCESS ");
+                event.dismiss();
+                break;
+        }
     }
-  }
 }
