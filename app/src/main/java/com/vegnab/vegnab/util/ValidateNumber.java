e9db809888bbc571e257a6896da8e3f9b9e24e55
Rick Shory <rickshory@gmail.com>
2016-12-01 08:44:23 -0800

    begin ValidateNumber class

---
+++ app/src/main/java/com/vegnab/vegnab/util/ValidateNumber.java
@@ -0,0 +1,13 @@
+package com.vegnab.vegnab.util;
+
+/**
+ * Created by rshory on 12/1/2016.
+ */
+
+public class ValidateNumber {
+
+    public ValidateNumber (String stringToValidate, float minAllowed, float maxAllowed,
+                    String problemIfEmpty, String problemIfNAN, String problemIfOutOfRange) {
+
+    }
+}
