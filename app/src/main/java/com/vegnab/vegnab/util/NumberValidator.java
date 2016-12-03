70cd9b81ae9a305599a6c5931a57f6d8e778bc2a
Rick Shory <rickshory@gmail.com>
2016-12-03 08:12:13 -0800

    work on NumberValidator constructors

--- app/src/main/java/com/vegnab/vegnab/util/NumberValidator.java
+++ app/src/main/java/com/vegnab/vegnab/util/NumberValidator.java
@@ -5,22 +5,38 @@ package com.vegnab.vegnab.util;
  */
 
 public class NumberValidator {
-    String mNumberToCheck = null;
-    String mMsgIfMissing = "no value";
-    boolean mTreatAsFloat = false;
-    String mMsgIfInvalid = "invalid value";
-    boolean mCheckRange = false;
-    int mMinInt, mMaxInt;
-    float mMinFloat, mMaxFloat;
-    String mMsgIfOutOfRange = "value out of range";
-    boolean mCheckSpecialValue = false;
-    int mSpecialInt;
-    float mSpecialFloat;
-    String mMsgSpecialValue = "value is special";
+    public String mNumberToCheck = null;
+    public String mMsgIfMissing = "no value";
+    public boolean mValueIsMissing = false;
+    private boolean mTreatAsFloat = false;
+    public boolean mValueInvalid = false;
+    public String mMsgIfInvalid = "invalid value";
+    private boolean mCheckRange = false;
+    public int mMinInt, mMaxInt;
+    public float mMinFloat, mMaxFloat;
+    public String mMsgIfOutOfRange = "value out of range";
+    private boolean mCheckSpecialValue = false;
+    public int mSpecialInt;
+    public float mSpecialFloat;
+    public String mMsgSpecialValue = "value is special";
+    public int mValidInt;
+    public float mValidFloat;
 
+    public NumberValidator(String stringToValidate) {
+        if ((stringToValidate == null) || (stringToValidate.trim().isEmpty())) {
+            mValueIsMissing = true;
+        } else {
+            try {
+                mValidInt = Integer.parseInt(stringToValidate.trim());
+            } catch (NumberFormatException e) {
+                mValueInvalid = true;
+            }
+        }
+    }
 
     public NumberValidator(String stringToValidate, float minAllowed, float maxAllowed,
                            String problemIfEmpty, String problemIfNAN, String problemIfOutOfRange) {
+
 
     }
 }
