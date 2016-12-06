a9d5849172f41216e13690c38d97230b65a0507a
Rick Shory <rickshory@gmail.com>
2016-12-06 09:10:20 -0800

    lost interest in NumberValidator, new class TableIdManager

---
+++ app/src/main/java/com/vegnab/vegnab/util/TableIdManager.java
@@ -0,0 +1,12 @@
+package com.vegnab.vegnab.util;
+
+/**
+ * Created by rshory on 12/6/2016.
+ * For database tables that have only 2 fields, [_id] and a text field, this class allows
+ *  doing something like n=getID("textString", "fieldName", "tableName")
+ *  If the text string is in the table, returns the existing ID,
+ *  If it is not yet in the table, adds a new record and returns that ID
+ */
+
+public class TableIdManager {
+}
