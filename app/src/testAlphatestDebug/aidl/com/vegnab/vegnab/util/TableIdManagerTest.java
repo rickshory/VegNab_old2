62b0003d3a2bb846794e7f5c2af072d0a9726b7d
Rick Shory <rickshory@gmail.com>
2016-12-15 08:16:41 -0800

    first try at Tests

---
+++ app/src/testAlphatestDebug/aidl/com/vegnab/vegnab/util/TableIdManagerTest.java
@@ -0,0 +1,26 @@
+package com.vegnab.vegnab.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by rshory on 12/15/2016.
+ */
+public class TableIdManagerTest {
+    @Test
+    public void onCreateLoader() throws Exception {
+
+    }
+
+    @Test
+    public void onLoadFinished() throws Exception {
+
+    }
+
+    @Test
+    public void onLoaderReset() throws Exception {
+
+    }
+
+}
