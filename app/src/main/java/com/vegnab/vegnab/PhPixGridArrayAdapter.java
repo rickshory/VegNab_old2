e17422087df5d61ff111b9cecb2cf54e822233d8
Rick Shory <rickshory@gmail.com>
2017-05-14 06:47:44 -0700

    convert bindView (cursor) to getView (list)

--- app/src/main/java/com/vegnab/vegnab/PhPixGridArrayAdapter.java
+++ app/src/main/java/com/vegnab/vegnab/PhPixGridArrayAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.support.v4.widget.ResourceCursorAdapter;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -36,11 +35,6 @@ public class PhPixGridArrayAdapter extends ArrayAdapter<VNGridImageItem> {
 //	private int layoutResourceId;
 //	private ArrayList data = new ArrayList();
 //
-//  private view holder class
-    private class ViewHolder {
-        ImageView imageView;
-        TextView txtTitle;
-    }
 
 //	public GridViewAdapter(Context context, int layoutResourceId, ArrayList data) {
 //		super(context, layoutResourceId, data);
@@ -49,37 +43,76 @@ public class PhPixGridArrayAdapter extends ArrayAdapter<VNGridImageItem> {
 //		this.data = data;
 //	}
 //
-    /*        public View getView(int position,View ConvertView,ViewGroup parent)
-        {
-            LayoutInflater inflater=(LayoutInflater)c.getSystemService(LAYOUT_INFLATER_SERVICE);
-            ConvertView =inflater.inflate(R.layout.forgridmain, parent, false);
-            ImageView iv1=(ImageView) ConvertView.findViewById(R.id.imageView111);
-            TextView tv=(TextView)ConvertView.findViewById(R.id.textViewgd1);
-            RelativeLayout rl=(RelativeLayout)ConvertView.findViewById(R.id.relalypout);
-            rl.setBackgroundColor(Color.TRANSPARENT);
-            iv1.setImageURI(Uri.parse("android.resource://"+getPackageName()+"/drawable/"+img[position]));
-            tv.setText(p[position]);
-            gv.setOnItemClickListener(ga);
-            return ConvertView;
-
-        }*/    // break the following apart to use with cursor instead of array
+    // break the following apart to use with cursor instead of array
 //	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-        View v = convertView;
-        if (v == null) {
-            LayoutInflater vi = LayoutInflater.from(getContext());
-            v = vi.inflate(R.layout.news_list_item, parent,false);
-
-            bindView(position, v);
-        } else
-        {
-            bindView(position, v);
-        }
-        return v;
-        ViewHolder holder = null;
+        
+        String imagePath = null;
         VNGridImageItem gridItem = getItem(position);
         LayoutInflater mInflater = (LayoutInflater)
                 ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.grid_ph_pix, null);
+            // get the title
+            TextView phGridCellText = (TextView) convertView.findViewById(R.id.phGridItemText);
+            String note = gridItem.getTitle();
+            if (note == null) {
+                note = "(no note)";
+            }
+            phGridCellText.setText(note);
+
+            // get the path and use it as this view's tag
+            imagePath = gridItem.getPath();
+            convertView.setTag(imagePath);
+
+            // get the image thumbnail
+
+            ImageView phGridCellImage = (ImageView) convertView.findViewById(R.id.phGridItemImage);
+            File imgFile = new  File(imagePath);
+            if (imgFile.exists()) {
+                // There isn't enough memory to open up more than a few camera photos
+                // so pre-scale the target bitmap into which the file is decoded
+                // Get the size of the ImageView
+                int targetW = phGridCellImage.getWidth();
+                int targetH = phGridCellImage.getHeight();
+                if (LDebug.ON) Log.d(LOG_TAG, "grid cell image Ht " + targetH + ", Wd " + targetW);
+                // for testing, manually override
+                targetW = 100;
+                targetH = 100;
+                // Get the size of the image
+                BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+                bmOptions.inJustDecodeBounds = true;
+                BitmapFactory.decodeFile(imagePath, bmOptions);
+                int photoW = bmOptions.outWidth;
+                int photoH = bmOptions.outHeight;
+                // determine the aspect ratio
+                int scaleFactor = 1;
+                if ((targetW > 0) || (targetH > 0)) {
+                    scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+                }
+                // Set bitmap options to scale the image decode target
+                bmOptions.inJustDecodeBounds = false;
+                bmOptions.inSampleSize = scaleFactor;
+                // bmOptions.inPurgeable = true;
+                // Decode the JPEG file into a Bitmap
+                if (LDebug.ON) Log.d(LOG_TAG, "Scale Factor " + scaleFactor + ", About to decode: " + imagePath);
+                Bitmap bitmap = BitmapFactory.decodeFile(imagePath, bmOptions);
+                if (LDebug.ON) Log.d(LOG_TAG, "bitmap Ht " + bitmap.getHeight() + ", width " + bitmap.getWidth());
+
+                // associate the Bitmap to the ImageView
+                phGridCellImage.setImageBitmap(bitmap);
+
+//            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+//            phGridCellImage.setImageBitmap(myBitmap);
+//            phGridCellImage.setAdjustViewBounds(true);
+            } else {
+                // set bitmap to a not-found icon
+            }
+        } else {
+            // convertView was already laid out
+            imagePath = (String) convertView.getTag();
+        }
+        return convertView;
 
     // adapt the following for newView
 //		if (row == null) {
@@ -99,70 +132,6 @@ public class PhPixGridArrayAdapter extends ArrayAdapter<VNGridImageItem> {
 //		holder.image.setImageBitmap(item.getImage());
 //		return row;
 	}
-//
-//	static class ViewHolder {
-//		TextView imageTitle;
-//		ImageView image;
-//	}
-
-    static class ViewHolder {
-        ImageView image;
-        TextView note;
-    }
-
-    @Override
-    public void bindView(View v, Context ctx, Cursor c) {
-        // available fields: PhotoPath, PlaceHolderID, PhotoTimeStamp, PhotoNotes, PhotoURL
-        TextView phGridCellText = (TextView) v.findViewById(R.id.phGridItemText);
-        String note = c.getString(c.getColumnIndexOrThrow("PhotoNotes"));
-        if (note == null) {
-            note = "(no note)";
-        }
-        phGridCellText.setText(note);
-
-        ImageView phGridCellImage = (ImageView) v.findViewById(R.id.phGridItemImage);
-        String path = c.getString(c.getColumnIndexOrThrow("PhotoPath"));
-        File imgFile = new  File(path);
-        if (imgFile.exists()) {
-            // There isn't enough memory to open up more than a few camera photos
-            // so pre-scale the target bitmap into which the file is decoded
-            // Get the size of the ImageView
-            int targetW = phGridCellImage.getWidth();
-            int targetH = phGridCellImage.getHeight();
-           if (LDebug.ON) Log.d(LOG_TAG, "grid cell image Ht " + targetH + ", Wd " + targetW);
-            // for testing, manually override
-            targetW = 100;
-            targetH = 100;
-            // Get the size of the image
-            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-            bmOptions.inJustDecodeBounds = true;
-            BitmapFactory.decodeFile(path, bmOptions);
-            int photoW = bmOptions.outWidth;
-            int photoH = bmOptions.outHeight;
-            // determine the aspect ratio
-            int scaleFactor = 1;
-            if ((targetW > 0) || (targetH > 0)) {
-                scaleFactor = Math.min(photoW/targetW, photoH/targetH);
-            }
-            // Set bitmap options to scale the image decode target
-            bmOptions.inJustDecodeBounds = false;
-            bmOptions.inSampleSize = scaleFactor;
-            // bmOptions.inPurgeable = true;
-            // Decode the JPEG file into a Bitmap
-           if (LDebug.ON) Log.d(LOG_TAG, "Scale Factor " + scaleFactor + ", About to decode: " + path);
-            Bitmap bitmap = BitmapFactory.decodeFile(path, bmOptions);
-           if (LDebug.ON) Log.d(LOG_TAG, "bitmap Ht " + bitmap.getHeight() + ", width " + bitmap.getWidth());
-
-            // associate the Bitmap to the ImageView
-            phGridCellImage.setImageBitmap(bitmap);
-
-//            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-//            phGridCellImage.setImageBitmap(myBitmap);
-//            phGridCellImage.setAdjustViewBounds(true);
-        } else {
-            // set bitmap to a not-found icon
-        }
-    }
 
     /*
     @Override
